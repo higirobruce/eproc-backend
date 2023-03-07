748ecdc39b484c588e8f47fd2ed6564a6daa2f34
higirobruce <higirobru@gmail.com>
2023-03-07 14:30:54 +0200

    Updates
    Pull Fixed Assets from B1

--- dist/services/b1.js
+++ dist/services/b1.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getBusinessPartnerByName = exports.getVatGroups = void 0;
+exports.getBusinessPartnerByName = exports.getFixedAssets = exports.getVatGroups = void 0;
 const express_1 = require("express");
 const node_localstorage_1 = require("node-localstorage");
 const sapB1Connection_1 = require("../utils/sapB1Connection");
@@ -17,6 +17,11 @@ let localstorage = new node_localstorage_1.LocalStorage("./scratch");
 let b1Router = (0, express_1.Router)();
 b1Router.get("/vatGroups", (req, response) => __awaiter(void 0, void 0, void 0, function* () {
     yield getVatGroups().then(res => {
+        response.send(res);
+    });
+}));
+b1Router.get("/fixedAssets", (req, response) => __awaiter(void 0, void 0, void 0, function* () {
+    yield getFixedAssets().then(res => {
         response.send(res);
     });
 }));
@@ -44,6 +49,25 @@ function getVatGroups() {
     });
 }
 exports.getVatGroups = getVatGroups;
+function getFixedAssets() {
+    return (0, sapB1Connection_1.sapLogin)()
+        .then((res) => __awaiter(this, void 0, void 0, function* () {
+        let resJson = yield res.json();
+        let COOKIE = res.headers.get("set-cookie");
+        localstorage.setItem("cookie", `${COOKIE}`);
+        return fetch(`https://192.168.20.181:50000/b1s/v1/Items?$filter= ItemType eq 'itFixedAssets' and CapitalizationDate eq null &$select=ItemCode,ItemName`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Cookie: `${localstorage.getItem("cookie")}`,
+            },
+        }).then(res => res.json());
+    })).catch(err => {
+        return err;
+        console.log(err);
+    });
+}
+exports.getFixedAssets = getFixedAssets;
 function getBusinessPartnerByName(CardName) {
     return (0, sapB1Connection_1.sapLogin)()
         .then((res) => __awaiter(this, void 0, void 0, function* () {
--- services/b1.ts
+++ services/b1.ts
@@ -12,11 +12,19 @@ b1Router.get("/vatGroups", async (req, response) => {
   })
 });
 
+b1Router.get("/fixedAssets", async (req, response) => {
+  await getFixedAssets().then(res=>{
+    response.send(res)
+  })
+});
+
 b1Router.get('/businessPartner/:name', async (req,response)=>{
   await getBusinessPartnerByName(req.params.name).then(res=>{
     response.send(res.value)
   })
 })
+
+
 
 export function getVatGroups(){
   return sapLogin()
@@ -26,6 +34,29 @@ export function getVatGroups(){
       localstorage.setItem("cookie", `${COOKIE}`);
       return fetch(
         `https://192.168.20.181:50000/b1s/v1/VatGroups?$filter=Inactive eq 'tNO'&$select=Code,Name,Category`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Cookie: `${localstorage.getItem("cookie")}`,
+          },
+        }
+      ).then(res=>res.json())
+    }).catch(err=>{
+      return err
+      console.log(err)
+    })
+   
+}
+
+export function getFixedAssets(){
+  return sapLogin()
+    .then(async (res) => {
+      let resJson = await res.json();
+      let COOKIE = res.headers.get("set-cookie");
+      localstorage.setItem("cookie", `${COOKIE}`);
+      return fetch(
+        `https://192.168.20.181:50000/b1s/v1/Items?$filter= ItemType eq 'itFixedAssets' and CapitalizationDate eq null &$select=ItemCode,ItemName`,
         {
           method: "GET",
           headers: {
