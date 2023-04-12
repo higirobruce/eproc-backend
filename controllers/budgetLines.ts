790db108b5c5c45aa51cba7c4def68cb429bf563
higirobruce <higirobru@gmail.com>
2023-04-12 10:27:58 +0200

    Updates
    New Budget lines structure

--- controllers/budgetLines.ts
+++ controllers/budgetLines.ts
@@ -1,11 +1,24 @@
 import { BudgetLine } from "../classrepo/budgetLines";
 import { BudgetLineModel } from "../models/budgetLines";
+import { DepartmentModel } from "../models/departments";
 
 
 export async function getAllBudgetLines() {
+    let pipeline = [
+        {
+          '$lookup': {
+            'from': 'budgetlines', 
+            'localField': '_id', 
+            'foreignField': 'department', 
+            'as': 'budgetlines'
+          }
+        }
+      ]
     try {
-        let dpts = await BudgetLineModel.find();
-        return dpts;
+        // let dpts = await BudgetLineModel.find();
+        let _dpts = await DepartmentModel.aggregate(pipeline)
+
+        return _dpts;
     } catch (err) {
         return {
             error: true,
