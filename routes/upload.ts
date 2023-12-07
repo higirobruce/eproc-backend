f23c3c56e155561bfc2f3d1b25eb94f4adfbb680
Bruce Higiro <higirobru@gmail.com>
2023-12-07 20:13:31 +0200

    Update upload.ts

--- routes/upload.ts
+++ routes/upload.ts
@@ -202,7 +202,7 @@ uploadRouter.post("/paymentRequests/", (req, res) => {
         null,
         file.originalname.split(path.extname(file.originalname))[0] +
           "_" +
-          moment().format('DD-MMM-YYYY_h:m:s') +
+          moment().format('DD-MMM-YYYY_h:m:sa') +
           path.extname(file.originalname)
       );
     },
@@ -259,13 +259,13 @@ uploadRouter.post("/updatePaymentRequests/", (req, res) => {
 
   var upload = multer({ storage: storage }).single("file");
   upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      console.log(err);
-      return res.status(500);
-    } else if (err) {
-      console.log(err);
-      return res.status(500);
-    }
+    // if (err instanceof multer.MulterError) {
+    //   console.log(err);
+    //   return res.status(500).send(req.file);
+    // } else if (err) {
+    //   console.log(err);
+    //   return res.status(500).send(req.file);
+    // }
     return res.status(200).send(req.file);
   });
 });
