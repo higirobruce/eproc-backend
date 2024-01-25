b85ba44e54818d43814d078c1544fcde2e49a951
Bruce Higiro <higirobru@gmail.com>
2024-01-25 21:08:04 +0200

    Update users.ts

--- controllers/users.ts
+++ controllers/users.ts
@@ -647,7 +647,7 @@ export async function updateMyPassword(
 }
 
 export async function resetPassword(email: String) {
-  let user = null;
+  let user;
   try {
     let newPassword = generatePassword(8);
     user = await UserModel.findOneAndUpdate(
