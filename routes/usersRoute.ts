cc50232377ade17f6d9d114bac2deb69559b7141
Bruce Higiro <higirobru@gmail.com>
2024-02-02 22:27:16 +0200

    Update usersRoute.ts

--- routes/usersRoute.ts
+++ routes/usersRoute.ts
@@ -248,9 +248,9 @@ userRouter.put("/:id", async (req, res) => {
   let { id } = req.params;
   let { newUser } = req.body;
 
-  // let nUser = _.omit(newUser,'sapCode')
-  // console.log(nUser)
-  let updates = await updateUser(id, newUser);
+  let nUser = _.omit(newUser,'sapCode')
+  console.log(nUser)
+  let updates = await updateUser(id, nUser);
 
   logger.log({
     level: "info",
